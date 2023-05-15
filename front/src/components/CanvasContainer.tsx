f565fe2f679ecbc62ada888decec0eb172881859
Kévin PARIS <kevin.paris701@gmail.com>
2023-05-16 01:48:22 +0200

    added resize to three renderer

--- front/src/components/CanvasContainer.tsx
+++ front/src/components/CanvasContainer.tsx
@@ -1,5 +1,6 @@
 import { Spin } from 'antd'
-import React, { Suspense, useEffect, useRef } from 'react'
+import React, { Suspense, useEffect, useRef, useMemo, useState } from 'react'
+import { useResizeDetector } from 'react-resize-detector';
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
@@ -7,9 +8,13 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const CanvasContainer: React.FC = () => {
 
-    const sceneRef = useRef(null)
+    const sceneRef = useRef<HTMLInputElement>(null)
+    const [renderer, setRenderer] = useState<THREE.WebGLRenderer>()
+    const { width, height, ref: sizeref } = useResizeDetector();
 
-    useEffect(() => {
+
+
+    useMemo(() => {
         async function fetchGLTFData() {
             const response = await fetch('http://127.0.0.1:3500/project/sword')
             const data = await response.blob()
@@ -18,10 +23,13 @@ const CanvasContainer: React.FC = () => {
 
         async function loadScene() {
             const gltfData = await fetchGLTFData()
+            if (!sceneRef.current)
+                return
 
             const scene = new THREE.Scene()
             const camera = new THREE.PerspectiveCamera(70, 2, 1, 100);
             const renderer = new THREE.WebGLRenderer({ antialias: true });
+            setRenderer(renderer)
             renderer.setSize(sceneRef.current.clientWidth, sceneRef.current.clientHeight)
 
             const controls = new OrbitControls(camera, renderer.domElement)
@@ -54,9 +62,18 @@ const CanvasContainer: React.FC = () => {
         loadScene()
     }, [])
 
+    useEffect(() => {
+        console.log(width, height)
+        if (!sceneRef.current)
+            return
+        renderer?.setSize(sceneRef.current.clientWidth, sceneRef.current.clientHeight)
+    }, [width, height])
+
     return (
         <Suspense fallback={<Spin />}>
-            <div ref={sceneRef} id='canvas-container' style={{ height: '100%', width: '100%' }}></div>
+            <div ref={sizeref} style={{ width: '100%', height: '100%', display: 'block' }}>
+                <div ref={sceneRef} id='canvas-container' style={{ width: '100%', height: '100%', display: 'block' }}></div>
+            </div>
         </Suspense>
     )
 }
